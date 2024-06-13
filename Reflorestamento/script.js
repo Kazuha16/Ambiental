2613dab8618025cac8f5c060cce2f8b9fc046a6e
SilverTH26 <106279736+Kazuha16@users.noreply.github.com>
2024-06-13 14:09:40 -0300

    Código em progresso

--- Reflorestamento/script.js
+++ Reflorestamento/script.js
@@ -34,6 +34,11 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for()
 }
 
 mostraPergunta();
