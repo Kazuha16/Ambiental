fce2ac0e5bdaad9ea5dd48196c2b34907d52ed3a
SilverTH26 <106279736+Kazuha16@users.noreply.github.com>
2024-06-13 13:52:59 -0300

    adição de perguntas e mudança de título e pastas

---
+++ Reflorestamento/script.js
@@ -0,0 +1,39 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Você possui conhecimento sobre o conceito de reflorestamento?",
+        alternativas: [
+            "Sim, tenho uma ideia geral.",
+            "Não, desconheço esse termo."
+        ]
+    },
+    {
+        enunciado: "Você contribui ou já pensou em contribuir no reflorestamento? Por que?",
+        alternativas: [
+            "Sim, contribuo ou penso em contribuir para desempenhar um papel importante no combate ao efeito estufa.",
+            "Não, não contribuo ou nunca pensei em contribuir, pois não vejo importância na prática ou acho perca de tempo.",
+        ]
+    },
+    {
+        enunciado: "Como você acha que deve ser feito o reflorestamneto?", 
+        alternativas: [
+            "Através do plantio de mudas em uma área devastada.",
+            "Por meio da inserção de espécies vegetais específicas que podem tornar o ambiente favorável para outras espécies ocuparem."
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
